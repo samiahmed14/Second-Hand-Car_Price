4583eb3763c66c73a74b1c9e08485397ccb91977
samiahmed14 <121189859+samiahmed14@users.noreply.github.com>
2022-12-27 00:21:10 +0530

    Commiting Car Project Core Base

---
+++ README.md
@@ -0,0 +1,43 @@
+# Second-Hand-Car-Price
+
+**Project overview**
+
+Car Price Prediction using Machine Learning is especially helpful when the vehicle is used and not coming direct from the factory. With increase in demand for used cars more and more vehicle buyers are finding alternatives of buying new cars. There is a need of accurate price prediction mechanism for the used cars. Prediction techniques of machine learning can be helpful in this regard.
+
+
+## Tech Stack
+
+Front end: HTML, CSS, BootStrap
+Backend: Flask, Nodejs
+Languages: Python, Javascript
+Model : Random Forest Regression
+Ide: VS code
+Other libraries : Pandas, Numpy, Seaborn, Matplotlib, Sklearn
+Data set : https://www.kaggle.com/datasets/saisaathvik/used-cars-dataset-from-cardekhocom
+
+
+
+![assd](https://github.com/faseehahmed26/Second-Hand-Car-Price/blob/main/ss/car_price.png?raw=true)
+## Application Features
+Step1: Data Cleaning
+
+Fixing or removing incorrect, corrupted, incorrectly formatted, duplicate, or incomplete data within a dataset. 
+
+Step2: Extracting Data Analysis
+
+Extracting important parameters and relationships that hold between them and to test underlying assumptions
+
+Step3: Feature Engineering
+
+assigning attribute-value pairs to a dataset that's stored as a table. 
+
+Step4: Model Training
+
+Identifying and Fitting the best combination of training data and providing good prediction range.
+
+![asd](https://github.com/faseehahmed26/Second-Hand-Car-Price/blob/main/ss/Picture1.png?raw=true)
+
+
+
+
+If you would like to experiment with the [dataset](https://www.kaggle.com/datasets/saisaathvik/used-cars-dataset-from-cardekhocom) yourself and the code at [Github](https://github.com/faseehahmed26/Second-Hand-Car-Price) the web app is deployed [Here](https://secondhand-car-price.herokuapp.com/) .
---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+import os
+target='random_forest_regression.pkl'
+'''
+model = {} # scores is an empty dict already
+
+if os.path.getsize(target) > 0:      
+    with open(target, "rb") as f:
+        unpickler = pickle.Unpickler(f)
+        # if file is not empty scores will be equal
+        # to the value unpickled
+        model = unpickler.load()
+'''
+model = pickle.load(open(target,'rb'))
+
+#model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Kms_Driven2=np.log(Kms_Driven)
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol=='Petrol'):
+                Fuel_Type_Petrol=1
+                Fuel_Type_Diesel=0
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=1
+        Year=2021-int(Year)
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+        else:
+            Seller_Type_Individual=0	
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        if(Transmission_Mannual=='Mannual'):
+            Transmission_Mannual=1
+        else:
+            Transmission_Mannual=0
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('predict.html',prediction_text="You Can Sell The Car at {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
